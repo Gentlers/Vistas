b0b1169376e79ce131479540d40b3b1926264236
haglermgp <hagler.mijail@gmail.com>
2016-12-28 20:36:27 -0500

    Avances

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+//Gruntfile.js
+module.exports = function (grunt) {
+  grunt.initConfig({
+
+    // Watch task config con Sass
+    watch: {
+      sass: {
+        files: "sass/*.sass",
+        tasks: ['sass']
+      }
+    },
+    // Sass task config
+    sass: {
+        dev: {
+            files: {
+                // fichero destino  // fichero .scss
+                "css/index.css" : "sass/index.sass",
+                "css/inicio.css" : "sass/inicio.sass",
+                "css/registro.css" : "sass/registro.sass",
+                "css/explorar.css" : "sass/explorar.sass",
+                "css/generales.css" : "sass/generales.sass"
+            }
+        }
+    },
+    // BrowserSync task config
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "css/*.css",
+            "js/*.js",
+            "*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          proxy: "http://gento/"
+          }
+        }
+      }
+
+    });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+ //Tarea por defecto
+ grunt.registerTask('default', ['browserSync', 'watch']);
+};
---
+++ js/inicio.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.slider').slider({full_width: true, height:500, indicators:false});
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+});
